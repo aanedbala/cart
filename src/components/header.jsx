c4e4d1e0961ab20f06d28c70ff3c553567ab4ae9
aanedbala <aanedbala@gmail.com>
2025-05-29 21:24:06 +0300

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return <nav className="pink lighten-3">
+        <div className="container">
+            <div className="nav-wrapper">
+                <a href="https://github.com/aanedbala/cart"
+                className="brand-logo">React Shop</a>
+                <ul id="nav-mobile" className="right
+                hide-on-med-and-down">
+                <li><a href="https://github.com/aanedbala/cart">Repository</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+}
+export { Header };
